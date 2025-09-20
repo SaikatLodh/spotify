bcaaba05706d20cf034462dda7e37406e7ac3803
SaikatLodh <saikatlodh8@gmail.com>
2025-09-20 20:57:19 +0530

    fixed some error

--- server/app/config/redis.ts
+++ server/app/config/redis.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
-const redis = new Redis();
+const redis = new Redis(process.env.REDIS_URL!);
 
 redis.on("connect", () => console.log("Redis connected"));
 redis.on("error", (err) => console.error("Redis error", err));
